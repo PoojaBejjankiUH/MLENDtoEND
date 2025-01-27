6c350b1d11636356f9d31722b57ecf5c63488379
PoojaBejjanki <pbejjanki@uh.edu>
2025-01-27 16:24:37 -0600

    second commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+# responsible for ml app initializations
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requireents
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+
+name='mlproject',
+version ='0.0.1',
+author="Pooja",
+author_email='pbejjanki@uh.edu',
+packages=find_packages(),
+install_requires=get_requirements('requirement.txt')
+
+
+)
